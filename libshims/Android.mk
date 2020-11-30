837fa3826986cbb09e7782f6628baa9c9e85f63a
royals <67448300+ritiksaini5464@users.noreply.github.com>
2020-11-30 09:28:36 +0530

    Update Android.mk

--- libshims/Android.mk
+++ libshims/Android.mk
@@ -37,22 +37,4 @@ LOCAL_PROPRIETARY_MODULE := true
 
 include $(BUILD_SHARED_LIBRARY)
 
-include $(CLEAR_VARS)
 
-LOCAL_SRC_FILES := \
-	strdup16to8.cpp \
-	strdup8to16.cpp
-
-LOCAL_SHARED_LIBRARIES := libcutils
-LOCAL_MODULE := libcutils_shim
-LOCAL_MODULE_TAGS := optional
-
-include $(BUILD_SHARED_LIBRARY)
-
-include $(CLEAR_VARS)
-LOCAL_SRC_FILES := camera_shim.cpp
-LOCAL_SHARED_LIBRARIES := libui libutils
-LOCAL_MODULE := camera_shim
-LOCAL_MODULE_TAGS := optional
-LOCAL_PROPRIETARY_MODULE := true
-include $(BUILD_SHARED_LIBRARY)
